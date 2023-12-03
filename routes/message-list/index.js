21909a04181444ed854d3a5b54acc9eaf17b2189
rwade4 <98890455+rwade4@users.noreply.github.com>
2023-12-02 23:31:52 -0500

    Delete Functionality
    Added the new delete functionality for milestone 3.

--- routes/message-list/index.js
+++ routes/message-list/index.js
@@ -1,7 +1,9 @@
 'use strict'
+const { fetch } = require('undici');
 const fastify = require('fastify')();
 
 const {getData} = require('../../utils/smm-list-data.js');
+const {deleteData} = require('../../utils/smm-delete-data.js');
 
 module.exports = async function (fastify, opts) {
     fastify.route({
@@ -11,6 +13,18 @@ module.exports = async function (fastify, opts) {
             const data = {...await getData()};
             console.log(data);
             return reply.status(200).view('./pages/smm-list/index.pug', data);
+        }
+    })
+
+    fastify.route({
+        method: 'DELETE',
+        url: '/:id',
+        handler: async function (request, reply) {
+            const id = request.params.id;
+
+            const result = {...await deleteData(id)};
+            console.log(result);
+            reply.redirect(303, '/message-list');
         }
     })
 }
